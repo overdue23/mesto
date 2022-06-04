03d4f2890f05e807cb6db266733d69a70af48b96
mkurbonov22 <max13121999@gmail.com>
2022-06-04 21:58:58 +0300

    fix: исправить замечание после 2-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,24 @@
 let popupElement = document.querySelector('.popup'); //переменная всего popup
 let editProfile = document.querySelector('.profile__edit-button'); //переменная открыть редактирование профиля
 let closeButton = popupElement.querySelector('.popup__close-button'); //переменная закрыть редактирование
-let saveButton = document.querySelector('.popup__save-button'); //переменная кнопки сохранить
+//let saveButton = document.querySelector('.popup__save-button'); //переменная кнопки сохранить
 let userName = document.querySelector('.profile__title'); //переменная касса имени профиля на странице
 let userJop = document.querySelector('.profile__description'); //переменная касса работы на странице
 
 let nameInput = document.querySelector('.popup__user-name'); // Воспользуйтесь инструментом .querySelector()
 let jobInput = document.querySelector('.popup__user-jop'); // Воспользуйтесь инструментом .querySelector()
 
+let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+
 
 function openPopup() {
-	popupElement.classList.remove('popup_opened');
+	popupElement.classList.add('popup_opened');
+	nameInput.value = userName.textContent;
+	jobInput.value = userJop.textContent;
 }
 
 function closePopup() {
-	popupElement.classList.add('popup_opened');
+	popupElement.classList.remove('popup_opened');
 }
 
 editProfile.addEventListener('click', openPopup)
@@ -22,9 +26,7 @@ closeButton.addEventListener('click', closePopup)
 
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
-nameInput.value = userName.textContent;
-jobInput.value = userJop.textContent;
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
